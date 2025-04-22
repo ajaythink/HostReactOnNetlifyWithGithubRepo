85dde055c958a76c3aef5225a77c8b258b74a912
Ajay Kumar <ajaykumar286458@gmail.com>
2025-04-22 07:59:15 +0530

    some text modify

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Testing to Deploy github repo on netlify</h1>
+      <h1>Testing to Deploy github repo on netlify </h1>
+      <h1>Thank you! for your support! </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
